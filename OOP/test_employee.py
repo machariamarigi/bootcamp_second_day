16d4d1cd6eac2886774990814a3def0993493173
machariamarigi <machariamarigi@gmail.com>
2017-06-13 14:10:15 +0300

    Add test for a method pay_rise responsible for increasing employee pay

--- OOP/test_employee.py
+++ OOP/test_employee.py
@@ -6,13 +6,19 @@ from employee import Employee
 class EmployeeTestCases(unittest.TestCase):
     """Class to test Employee class"""
     def setUp(self):
-        self.employee = Employee('Test', 'Name', 0)
+        self.employee = Employee('Test', 'Name', 100)
 
     def test_employee_instance(self):
         self.assertEqual(
             self.employee.name,
             'Test Name',
             'Class not instantiating properly')
+
+    def test_employee_pay_rise(self):
+        initial_pay = self.employee.pay
+        self.employee.pay_rise()
+        final_pay = self.employee.pay
+        self.assertEqual(final_pay, 105)
 
 
 if __name__ == "__main__":
