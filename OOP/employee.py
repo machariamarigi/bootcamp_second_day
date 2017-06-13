9649bf798b713b64658af70535e30efdd010cd08
machariamarigi <machariamarigi@gmail.com>
2017-06-13 14:17:24 +0300

    Add the pay_rise method to code refactored it to pass tests

--- OOP/employee.py
+++ OOP/employee.py
@@ -4,8 +4,13 @@
 class Employee(object):
     """Base Class for an Employee"""
 
+    raise_amount = 1.05
+
     def __init__(self, firstname, lastname, pay):
         self.firstname = firstname
         self.lastname = lastname
         self.name = "{0} {1}".format(firstname, lastname)
         self.pay = pay
+
+    def pay_rise(self):
+        self.pay = self.pay * self.raise_amount
