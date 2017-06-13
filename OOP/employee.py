8a5fa9dac977a2c0a6c4290919b5879a153d6b25
machariamarigi <machariamarigi@gmail.com>
2017-06-13 13:58:59 +0300

    Refactored Employee class to instantiate properly

--- OOP/employee.py
+++ OOP/employee.py
@@ -3,4 +3,9 @@
 
 class Employee(object):
     """Base Class for an Employee"""
-    pass
+
+    def __init__(self, firstname, lastname, pay):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.name = "{0} {1}".format(firstname, lastname)
+        self.pay = pay
